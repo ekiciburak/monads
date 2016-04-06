47c2e486336c6324e8e67630a96bc88743a4cfbe
ekiciburak <ekcburak@hotmail.com>
2016-04-06 17:26:17 -0500

    better representation...

--- towards_monads.v
+++ towards_monads.v
@@ -2,124 +2,122 @@ Require Import Morphisms.
 Import ProperNotations.
 Require Import SetoidClass.
 
-Class Morp (X: Type) : Type := 
-  {
-     arrow: X -> X -> Type
-  }.
-Check Morp.
-
-Class Category (A: Type) `(@Morp A): Type := 
-  {
-     arrows_setoid :> forall a b, Setoid (arrow b a);
-     identity : forall {a: A}, arrow a a;
+Record Category: Type := 
+ mk_Category 
+ {
+     obj :> Type;
+     arrow: obj -> obj -> Type;
+     arrows_setoid :> forall {a b: obj}, Setoid (arrow b a);
+     identity : forall a, arrow a a;
      comp : forall {a b c}, (arrow a b) -> (arrow b c) -> (arrow a c);
      assoc : forall {a b c d} (f : arrow b a) (g : arrow c b) (h : arrow d c), comp h (comp g f) == comp (comp h g) f
   }.
-Check Category. 
 
-Notation " x 'o' y " := (comp x y) (at level 40, left associativity).
+Notation " x 'o' y " := (comp _ x y) (at level 40, left associativity). 
 
-Class Product (C: Type) (mc: Morp C) (catC: Category C mc) (p a b: C)
-              (pi1: arrow a p) (pi2: arrow b p) :=
+Record Product (catC: Category) (p a b: obj catC)
+               (pi1: arrow catC a p) (pi2: arrow catC b p): Type :=
+ mk_Product
   {
-    pcomm_diag: forall (x: C) (f1: arrow a x) (f2: arrow b x), exists !(g: arrow p x), f1 == pi1 o g /\ f2 == pi2 o g
+    parrows_setoid : forall {a b: catC}, Setoid (arrow catC b a);
+    pcomm_diag: forall (x: catC) (f1: arrow catC a x) (f2: arrow catC b x), 
+      exists !(g: arrow catC p x), f1 == pi1 o g /\ f2 == pi2 o g
   }.
+Check Product.
 
-Class coProduct (C: Type) (mc: Morp C) (catC: Category C mc) (cp a b: C)
-                (inl: arrow cp a) (inr: arrow cp b) :=
+Record coProduct (catC: Category) (cp a b: catC)
+                (inl: arrow catC cp a) (inr: arrow catC cp b): Type :=
   {
-    cpcomm_diag: forall (x: C) (f1: arrow x a) (f2: arrow x b), exists !(g: arrow x cp), f1 == g o inl /\ f2 == g o inr
+    cparrows_setoid : forall {a b: catC}, Setoid (arrow catC b a);
+    cpcomm_diag: forall (x: catC) (f1: arrow catC x a) (f2: arrow catC x b), 
+      exists !(g: arrow catC x cp), f1 == g o inl /\ f2 == g o inr
   }.
+Check coProduct.
 
-Class Pullback (C: Type) (mc: Morp C) (catC: Category C mc) (p a b c: C) 
-               (f: arrow c a) (g: arrow c b)
-               (pi1: arrow a p) (pi2: arrow b p)
-               (commd: g o pi2 == f o pi1) :=
+Record Pullback (catC: Category) (p a b c: catC) 
+               (f: arrow catC c a) (g: arrow catC c b)
+               (pi1: arrow catC a p) (pi2: arrow catC b p): Type :=
 {
-   pbcomm_diag: forall (q: C) (q1: arrow a q) (q2: arrow b q), exists !(u: arrow p q), pi2 o u == q2 /\ pi1 o u == q1
+   pbarrows_setoid : forall {a b: catC}, Setoid (arrow catC b a);
+   pbcomm_diag1: g o pi2 == f o pi1;
+   pbcomm_diag2: forall (q: catC) (q1: arrow catC a q) (q2: arrow catC b q), 
+     exists !(u: arrow catC p q), pi2 o u == q2 /\ pi1 o u == q1
 }.
+Check Pullback.
 
-Class Pushout (C: Type) (mc: Morp C) (catC: Category C mc) (p a b c: C) 
-               (f: arrow a c) (g: arrow b c)
-               (inl: arrow p a) (inr: arrow p b)
-               (commd: inr o g == inl o f) :=
+Record Pushout  (catC: Category) (p a b c: catC) 
+               (f: arrow catC a c) (g: arrow catC b c)
+               (inl: arrow catC p a) (inr: arrow catC p b): Type :=
 {
-   pocomm_diag: forall (q: C) (q1: arrow q a) (q2: arrow q b), exists !(u: arrow q p), u o inr == q2 /\ u o inl == q1
+   poarrows_setoid : forall {a b: catC}, Setoid (arrow catC b a);
+   pocomm_diag1: inr o g == inl o f;
+   pocomm_diag2: forall (q: catC) (q1: arrow catC q a) (q2: arrow catC q b), 
+     exists !(u: arrow catC q p), u o inr == q2 /\ u o inl == q1
 }.
+Check Pushout.
 
-Class Functor (C D: Type) (mc: Morp C) (catC: Category C mc) 
-                          (md: Morp D) (catD: Category D md)
-              (F: C -> D)
-              (fmap : forall {a b: C} (f: arrow b a), (arrow (F b) (F a))) :=
+Class Functor (catC catD: Category) (F: catC -> catD)
+              (fmap : forall {a b: catC} (f: arrow catC b a), (arrow catD (F b) (F a))): Type :=
   {
-    preserve_id : forall {a: C}, fmap (@identity C mc catC a) == (@identity D md catD (F a));
-    preserve_comp : forall {a b c: C} (f: arrow b a) (g : arrow c b), fmap (g o f) == (fmap g) o (fmap f)
+    farrows_setoidC : forall {a b: catC}, Setoid (arrow catC b a);
+    farrows_setoidD : forall {a b: catD}, Setoid (arrow catD b a);
+    preserve_id     : forall {a: catC}, fmap (@identity catC a) == (@identity catD (F a));
+    preserve_comp   : forall {a b c: catC} (f: arrow catC b a) (g : arrow catC c b), fmap (g o f) == (fmap g) o (fmap f)
   }.
 Check Functor.
 
-Definition Product_Functor (C: Type) (mc: Morp C) (catC: Category C mc) 
-                                     (mcc: Morp (C * C)) (catCC: Category (C * C) mcc)
-                           (F: C -> (C * C)) 
-                                     (fmapF : forall (a b: C) (f: arrow b a), (arrow (F b) (F a))) :=
-                                     `(@Functor C (C*C) mc catC mcc catCC F fmapF).
+Definition coq_id_on_objects (catC: Category) (a: catC) := a.
+Definition coq_id_on_morphisms (catC: Category) (a b: catC) (f: (@arrow catC b a)) := f.
+Check coq_id_on_morphisms.
 
-Definition coProduct_Functor (C: Type) (mc: Morp C) (catC: Category C mc) 
-                                       (mcs: Morp (C + C)) (catCS: Category (C + C) mcs)
-                             (F: C -> (C + C)) 
-                                       (fmapF : forall (a b: C) (f: arrow b a), (arrow (F b) (F a))) :=
-                                       `(@Functor C (C+C) mc catC mcs catCS F fmapF).
+Program Instance ID (catC: Category): 
+                       (@Functor catC catC (coq_id_on_objects catC) (coq_id_on_morphisms catC)).
+Obligation 1. destruct catC. apply arrows_setoid0. Qed.
+Next Obligation. destruct catC. apply arrows_setoid0. Qed.
+Next Obligation. unfold coq_id_on_objects. reflexivity. Qed.
+Next Obligation. unfold coq_id_on_objects; reflexivity. Qed.
+Check ID.
 
- Definition coq_id_on_objects (A: Type) (a: A) := a.
- Definition coq_id_on_morphisms (C: Type) (mc: Morp C) (catC: Category C mc) (a b:C) (f: (@arrow C mc b a)) := f.
- Check coq_id_on_morphisms.
-
- Program Instance ID (C: Type) (mc: Morp C) (catC: Category C mc) : 
-                       (@Functor C C mc catC mc catC (coq_id_on_objects C) (coq_id_on_morphisms C mc catC)).
- Obligation 1. unfold coq_id_on_objects; reflexivity. Qed.
- Next Obligation. unfold coq_id_on_objects; reflexivity. Qed.
- Check ID.
-
-Class NaturalTransformation (C D: Type)  (mc: Morp C) (catC: Category C mc) 
-                                         (md: Morp D) (catD: Category D md)
-                            (F G: C -> D) 
-                                         (fmapF : forall (a b: C) (f: arrow b a), (arrow (F b) (F a)))
-                                         (fmapG : forall (a b: C) (f: arrow b a), (arrow (G b) (G a)))
-                                         `(@Functor C D mc catC md catD F fmapF) 
-                                         `(@Functor C D mc catC md catD G fmapG) 
-                            (trans : forall (a: C), (arrow (G a) (F a))) :=
+Class NaturalTransformation (catC catD: Category) (F G: catC -> catD) 
+                            (fmapF : forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
+                            (fmapG : forall (a b: catC) (f: arrow catC b a), (arrow catD (G b) (G a)))
+                            `(@Functor catC catD F fmapF) 
+                            `(@Functor catC catD G fmapG) 
+                                                  (trans : forall (a: catC), (arrow catD (G a) (F a))): Type :=
   {
-    comm_diag: forall {a b: C} (f: arrow b a), fmapG _ _ f o trans a == trans b o fmapF _ _ f
+    nt_setoid1: forall {a b: catC}, Setoid (arrow catC b a);
+    nt_setoid2: forall {a b: catD}, Setoid (arrow catD b a);
+    comm_diag: forall {a b: catC} (f: arrow catC b a), fmapG _ _ f o trans a == trans b o fmapF _ _ f
   }.
 Check NaturalTransformation.
 
-Class Monad (C: Type) (mc: Morp C) (catC: Category C mc) (F: C -> C)
-                      (fmapId : forall (a b: C) (f: arrow b a), (arrow (id b) (id a)))
-                      (fmapT  : forall (a b: C) (f: arrow b a), (arrow (F b) (F a)))
-                      (fmapT2 : forall (a b: C) (f: arrow b a), (arrow (F (F b)) (F (F a))))
-                        (IdF : (Functor C C mc catC mc catC id fmapId))
-                        (T   : (Functor C C mc catC mc catC F fmapT))
-                        (T2  : (Functor C C mc catC mc catC (fun a: C => F (F a)) fmapT2))
-                        (eta : forall (a: C), (arrow (F a) (id a)))
-                        (mu  : forall (a: C), (arrow (F a) (F (F a))))
-                          `(@NaturalTransformation C C mc catC mc catC id F fmapId fmapT IdF T eta)
-                          `(@NaturalTransformation C C mc catC mc catC (fun a: C => F (F a)) F fmapT2 fmapT T2 T mu) :=
+Class Monad (catC: Category) (F: catC -> catC)
+            (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
+            (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
+            (fmapT2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
+                        (IdF : (Functor catC catC id fmapId))
+                        (T   : (Functor catC catC F fmapT))
+                        (T2  : (Functor catC catC (fun a: catC => F (F a)) fmapT2))
+                        (eta : forall (a: catC), (arrow catC (F a) (id a)))
+                        (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
+                          `(@NaturalTransformation catC catC id F fmapId fmapT IdF T eta)
+                          `(@NaturalTransformation catC catC (fun a: catC => F (F a)) F fmapT2 fmapT T2 T mu): Type :=
   {
-    comm_diagram1   : forall (a: C), (mu a) o (fmapT _ _  (mu a)) == (mu a) o (mu (F a));
-    comm_diagram2   : forall (a: C), (mu a) o (fmapT _ _ (eta a)) == (mu a) o (eta (F a));
-    comm_diagram2_b1: forall (a: C), (mu a) o (fmapT _ _ (eta a)) ==  identity;
-    comm_diagram2_b2: forall (a: C), (mu a) o (eta (F a)) ==  identity
+    m_setoid1: forall {a b: catC}, Setoid (arrow catC b a);
+    comm_diagram1   : forall (a: catC), (mu a) o (fmapT _ _  (mu a)) == (mu a) o (mu (F a));
+    comm_diagram2   : forall (a: catC), (mu a) o (fmapT _ _ (eta a)) == (mu a) o (eta (F a));
+    comm_diagram2_b1: forall (a: catC), (mu a) o (fmapT _ _ (eta a)) ==  (identity catC (F a));
+    comm_diagram2_b2: forall (a: catC), (mu a) o (eta (F a)) ==  (identity catC (F a))
   }.
 Check Monad.
 
-Class Adjunction (C D: Type)  (mc: Morp C) (catC: Category C mc) 
-                              (md: Morp D) (catD: Category D md)
-                 (F: C -> D) (G: D -> C) 
-                              (fmapF : forall (a b: C) (f: arrow b a), (arrow (F b) (F a)))
-                              (fmapG : forall (a b: D) (f: arrow b a), (arrow (G b) (G a)))
-                              `(@Functor C D mc catC md catD F fmapF) 
-                              `(@Functor D C md catD mc catC G fmapG) :=
+Class Adjunction (catC catD: Category) (F: catC -> catD) (G: catD -> catC) 
+                 (fmapF : forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
+                 (fmapG : forall (a b: catD) (f: arrow catD b a), (arrow catC (G b) (G a)))
+                 `(@Functor catC catD F fmapF) 
+                 `(@Functor catD catC G fmapG): Type :=
   {
-     bijl : forall (b: C) (a: D), (arrow a (F b)) -> (arrow (G a) b);
-     bijr : forall (b: C) (a: D), (arrow (G a) b) -> (arrow a (F b))
+     bijl : forall (b: catC) (a: catD), (arrow catD a (F b)) -> (arrow catC (G a) b);
+     bijr : forall (b: catC) (a: catD), (arrow catC (G a) b) -> (arrow catD a (F b))
   }.
 Check Adjunction.

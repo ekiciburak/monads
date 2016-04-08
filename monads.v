3e669abc28f6e36428d80bf8870f6a9fadb137cb
ekiciburak <ekcburak@hotmail.com>
2016-04-08 11:32:01 -0500

    moularizing.

--- monads.v
+++ monads.v
@@ -25,15 +25,4 @@ Class Monad (catC: Category) (F: catC -> catC)
   }.
 Check Monad.
 
-Class Adjunction (catC catD: Category) (F: catC -> catD) (G: catD -> catC) 
-                 (fmapF : forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
-                 (fmapG : forall (a b: catD) (f: arrow catD b a), (arrow catC (G b) (G a)))
-                 `(@Functor catC catD F fmapF) 
-                 `(@Functor catD catC G fmapG): Type :=
-  {
-     bijl : forall (b: catC) (a: catD), (arrow catD a (F b)) -> (arrow catC (G a) b);
-     bijr : forall (b: catC) (a: catD), (arrow catC (G a) b) -> (arrow catD a (F b))
-  }.
-Check Adjunction.
-
 End Make.

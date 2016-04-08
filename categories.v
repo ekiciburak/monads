bb7c406b9f91ae02b5bbb34b207fc9b3c4221799
ekiciburak <ekcburak@hotmail.com>
2016-04-08 13:52:07 -0500

    opposite functors added.

--- categories.v
+++ categories.v
@@ -19,47 +19,56 @@ Notation " x 'o' y " := (comp _ x y) (at level 40, left associativity).
 
 Definition Product_Category (catC catD: Category) : Category.
 Proof. 
-refine (@mk_Category 
-(catC * catD)%type
-(fun a b => (arrow catC (fst a) (fst b) * arrow catD (snd a) (snd b))%type)
-(fun a => (identity catC (fst a), identity catD (snd a)))
-(fun a b c f2 f1 => (fst f2 o fst f1, snd f2 o snd f1))
-_ ). intros. setoid_rewrite <- assoc. reflexivity.
-Qed.
+  refine (@mk_Category 
+           (catC * catD)%type
+           (fun a b => (arrow catC (fst a) (fst b) * arrow catD (snd a) (snd b))%type)
+           (fun a => (identity catC (fst a), identity catD (snd a)))
+           (fun a b c f2 f1 => (fst f2 o fst f1, snd f2 o snd f1))
+           _ 
+         ). 
+  intros. setoid_rewrite <- assoc. reflexivity.
+Defined.
 Check Product_Category.
 
 Definition Dual_Category (catC: Category) : Category.
 Proof. 
-refine (@mk_Category 
-(catC)%type
-(fun a b => (arrow catC b a %type))
-(fun a => (@identity catC a))
-(fun a b c f1 f2 => f2 o f1)
-_ ). intros. setoid_rewrite <- assoc. reflexivity.
-Qed.
+  refine (@mk_Category 
+           (catC)%type
+           (fun a b => (arrow catC b a %type))
+           (fun a => (@identity catC a))
+           (fun a b c f1 f2 => f2 o f1)
+           _
+         ). 
+  intros. setoid_rewrite <- assoc. reflexivity.
+Defined.
 Check Dual_Category.
 
 (*
-Class Category (Obj : Type) (Arrow: Obj -> Obj -> Type) : Type := 
+Class Category2 (Obj : Type) (Arrow: Obj -> Obj -> Type) : Type := 
  {
-     obj := Obj;
-     arrow := Arrow;
-     identity : forall a, arrow a a;
-     comp : forall {a b c}, (arrow a b) -> (arrow b c) -> (arrow a c);
-     assoc : forall {a b c d} (f : arrow b a) (g : arrow c b) (h : arrow d c), comp h (comp g f) = comp (comp h g) f
+     obj2 := Obj;
+     arrow2 := Arrow;
+     identity2 : forall a, arrow2 a a;
+     comp2 : forall {a b c}, (arrow2 a b) -> (arrow2 b c) -> (arrow2 a c);
+     assoc2 : forall {a b c d} (f : arrow2 b a) (g : arrow2 c b) (h : arrow2 d c), comp2 h (comp2 g f) = comp2 (comp2 h g) f
   }.
-Coercion obj : Category >-> Sortclass.
-Check comp.
 
-Notation " x 'o' y " := (comp x y) (at level 40, left associativity).
+Coercion obj2 : Category2 >-> Sortclass.
+Check comp2.
 
-Definition Dual_Category `(catC: Category Obj Arrow) : Category Obj (fun x y => Arrow y x).
-refine (@Build_Category Obj
-                              (fun x y => arrow y x) 
-                              (fun a => (@identity obj arrow catC a))
-                              (fun a b c f g => g o f )
+
+Notation " x 'O' y " := (comp2 x y) (at level 40, left associativity).
+
+Generalizable All Variables.
+
+Definition Dual_Category2 `(catC: Category2 objC arrowC) : Category2 objC (fun x y => arrowC y x).
+refine (@Build_Category2 objC
+                              (fun x y => arrow2 y x) 
+                              (fun a => (@identity2 obj2 arrow2 catC a))
+                              (fun a b c f g => g O f)
                              _).
-intros. rewrite assoc. reflexivity. Defined.
+intros. rewrite <- assoc2. reflexivity. Defined.
+Check Dual_Category2.
 *)
 
 Record Product (catC: Category) (p a b: obj catC)
--- functors.v
+++ functors.v
@@ -7,13 +7,28 @@ Module Make(Import M: notation.T).
  Module Export functors_exp := categories.Make(M).
 
 Class Functor `(catC: Category) `(catD: Category) (F: catC -> catD)
-              (fmap : forall {a b: catC} (f: arrow catC b a), (arrow catD (F b) (F a))): Type :=
+                (fmap: forall {a b: catC} (f: arrow catC b a), (arrow catD (F b) (F a))): Type :=
   mk_Functor
-  { 
+  {
     preserve_id     : forall {a: catC}, fmap (@identity catC a) = (@identity catD (F a));
     preserve_comp   : forall {a b c: catC} (f: arrow catC b a) (g : arrow catC c b), fmap (g o f) = (fmap g) o (fmap f)
   }.
 Check Functor.
+
+Definition Opposite_Functor `(catC: Category) `(catD: Category) 
+                            (F: catC -> catD)
+                            (fmapF: forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
+                                (FunctF: Functor catC catD F fmapF): (Functor (Dual_Category catC) (Dual_Category catD) F (fun a b => fmapF b a)).
+Proof. refine (@mk_Functor
+                (Dual_Category catC) 
+                (Dual_Category catD)
+                F
+                (fun a b => fmapF b a)
+                (fun a => (@preserve_id catC catD F fmapF FunctF a)) 
+                (fun a b c f g => (@preserve_comp catC catD F fmapF FunctF c b a g f))
+              ).
+Defined.
+Check Opposite_Functor.
 
 (*define how the identity functor behaves on objects and morphisms*)
 Definition coq_id_on_objects (catC: Category) (a: catC) := a.
@@ -21,7 +36,7 @@ Definition coq_id_on_morphisms (catC: Category) (a b: catC) (f: (@arrow catC b a
 Check coq_id_on_morphisms.
 
 Program Instance ID (catC: Category): 
-                       (@Functor catC catC (coq_id_on_objects catC) (coq_id_on_morphisms catC)).
+                       (@Functor catC catC (coq_id_on_objects catC)  (coq_id_on_morphisms catC)).
 (*
 Obligation 1. destruct catC. apply arrows_setoid0. Qed.
 Next Obligation. destruct catC. apply arrows_setoid0. Qed.
@@ -54,42 +69,5 @@ Next Obligation. unfold coq_comp_on_objects, coq_comp_on_morphism. remember (@pr
   remember (@preserve_comp catD catE G fmapG FunctG (F a) (F b) (F c) (fmapF _ _ f) (fmapF _ _ g)).
   rewrite <- e0. rewrite e. reflexivity. Defined.
 Check Compose_Functors.
-
-Generalizable All Variables.
-
-(*
-Definition func_op `(FunctF: Functor (catC := C) (catD := D) (F := fobj)): (Functor (Dual_Category D) (Dual_Category D) fobj).
-
-Class Functor `(catC: Category  objC arrowC) `( catD: Category  objD arrowD) (F: catC -> catD) :=
-  { 
-    fmap2            : forall {a b} (f: (@arrow objC arrowC catC b a)), (@arrow objD arrowD catD (F b) (F a));
-    preserve_id2     : forall {a: catC}, fmap2 (@identity objC arrowC catC a) = (@identity objD arrowD catD (F a));
-    preserve_comp2   : forall {a b c: catC} (f: (@arrow objC arrowC catC b a)) (g : (@arrow objC arrowC catC c b)), fmap2 (g o f) = (fmap2 g) o (fmap2 f)
-  }.
-Check Functor.
-
-Definition func_op `(FunctF: Functor (catC := C) (catD := D) (F := fobj)): (Functor (Dual_Category D) (Dual_Category D) fobj).
-
-Class Functor3 (catC catD: Category): Type :=
-  mk_Functor3
-  { 
-    omap            :> catC -> catD;
-    fmap3            : forall a b (f: arrow catC b a), (arrow catD (omap b) (omap a));
-    preserve_id3     : forall a, fmap3 _ _ (@identity catC a) = (@identity catD (omap a));
-    preserve_comp3   : forall a b c (f: arrow catC b a) (g : arrow catC c b), fmap3 _ _ (g o f) = (fmap3 _ _ g) o (fmap3 _ _ f)
-  }.
-
-Check Functor2.
-
-
-Definition Opposite_Functor (catC catD: Category) (F: Functor3 catC catD): Functor3 (Dual_Category catC) (Dual_Category catD):=
-  (@mk_Functor3 (Dual_Category catC)%type (Dual_Category catD)%type
-  (fun a => omap a)
-  (fun x y => fmap3 x y)
-  (fun x => preserve_id3 x)
-  (fun a b c f g => preserve_comp3 a b c f g)  
-).
-
-*)
 
 End Make.

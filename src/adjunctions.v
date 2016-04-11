bfc8f9d7f1a196253139ac6f45ad6a624d70e53a
ekiciburak <ekcburak@hotmail.com>
2016-04-11 14:56:10 -0500

    small modification regarding id's.

--- src/adjunctions.v
+++ src/adjunctions.v
@@ -6,7 +6,6 @@ Require notation categories prods_pullbacks functors natural_transformations mon
 Module Make(Import M: notation.T).
  Module Export adjunctions_exp := monads.Make(M).
 
-
 Class Adjunction (catC catD: Category) (F: catC -> catD) (G: catD -> catC) 
                  (fmapF : forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
                  (fmapG : forall (a b: catD) (f: arrow catD b a), (arrow catC (G b) (G a)))

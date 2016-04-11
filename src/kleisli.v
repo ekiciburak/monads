7807d527f0e7fef22cdd64d4fd496624013f2a0d
ekiciburak <ekcburak@hotmail.com>
2016-04-11 16:54:38 -0500

    Kleisli Categories is now  instances of the Category class.

--- src/kleisli.v
+++ src/kleisli.v
@@ -8,94 +8,52 @@ Module Make(Import M: notation.T).
 
 Definition id {catC: Category} (a: catC) := a.
 
-Definition Kleisli `(catC: Category) (F: catC -> catC)
-              (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
-              (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-              (fmapT2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (Id  : (Functor catC catC id fmapId))
-                        (T   : (Functor catC catC F fmapT))
-                        (T2  : (Functor catC catC (fun a: catC => F (F a)) fmapT2))
-                        (eta : forall (a: catC), (arrow catC (F a) (id a)))
-                        (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
-                          (nt1: NaturalTransformation catC catC id F fmapId fmapT Id T eta) 
-                          (nt2: NaturalTransformation catC catC (fun a: catC => F (F a)) F fmapT2 fmapT T2 T mu)
-                          `(@Monad catC F fmapId fmapT fmapT2 Id T T2 eta mu nt1 nt2) : (Category).
+Theorem rcancel:  forall {catC: Category} {a b c: catC} (f g: arrow _ c b) (h: arrow _ b a), f = g -> f o h = g o h.
+Proof. intros. rewrite H. reflexivity. Qed.
+
+Theorem lcancel:  forall {catC: Category} {a b c: catC} (f g: arrow _ b a) (h: arrow _ c b), f = g -> h o f = h o g.
+Proof. intros. rewrite H. reflexivity. Qed.
+
+Definition Kleisli_Category `(catC: Category) (F: catC -> catC)
+                            (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
+                            (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
+                               (Id  : (Functor catC catC id fmapId))
+                               (T   : (Functor catC catC F fmapT))
+                               (T2  : (Functor catC catC (fun a: catC => F (F a)) (fun a b f => fmapT _ _ (fmapT _ _ f))))
+                                   (eta : forall (a: catC), (arrow catC (F a) (id a)))
+                                   (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
+                                      (nt1: NaturalTransformation catC catC id F fmapId fmapT Id T eta) 
+                                      (nt2: NaturalTransformation catC catC (fun a: catC => F (F a)) F (fun a b f => fmapT _ _ (fmapT _ _ f)) fmapT T2 T mu)
+                                         (M: Monad catC F fmapId fmapT Id T T2 eta mu nt1 nt2) : (Category).
 Proof. refine (@mk_Category (catC)
                             (fun a b => (@arrow catC (F b) (id a)))
                             (fun a => eta a) 
                             (fun a b c f g => (mu (id c)) o (fmapT _ _ g) o f)
-_ ). intros. simpl. destruct nt1, nt2. do 2 rewrite assoc. unfold id in *. remember (comm_diag1 _ _ f).
-destruct H. admit. (* to be proven *) Qed.
-Check Kleisli.
+                            _ ).
+       intros. simpl. destruct nt1, nt2, M, T, T2. unfold adjunctions_exp.id, id in *. rewrite preserve_comp0.
+       rewrite assoc. rewrite preserve_comp0. rewrite assoc. rewrite (comm_diagram3 a). do 2 rewrite assoc.
+       specialize(@comm_diag1 b (F a) f). rewrite <- assoc. (* rewrite comm_diag1. ?? *) rewrite assoc.
+       apply rcancel. apply rcancel. rewrite <- assoc.
+       rewrite <- assoc. apply lcancel. rewrite comm_diag1. reflexivity. Qed.
+Check Kleisli_Category.     
 
-Definition coKleisli (catC: Category) (F: catC -> catC)
-                (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
-                (fmapD  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-                (fmapD2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (Id      : (Functor catC catC id fmapId))
-                        (D       : (Functor catC catC F fmapD))
-                        (D2      : (Functor catC catC (fun a: catC => F (F a)) fmapD2))
-                        (epsilon : forall (a: catC), (arrow catC (id a) (F a)))
-                        (delta   : forall (a: catC), (arrow catC (F (F a)) (F a)))
-                          (nt1: NaturalTransformation catC catC F id fmapD fmapId D Id epsilon)
-                          (nt2: NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD fmapD2 D D2 delta)
-                            `(@coMonad catC F fmapId fmapD fmapD2 Id D D2 epsilon delta nt1 nt2) : (Category).
+Definition coKleisli_Category (catC: Category) (F: catC -> catC)
+                              (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
+                              (fmapD  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
+                                 (Id  : (Functor catC catC id fmapId))
+                                 (D   : (Functor catC catC F fmapD))
+                                 (D2  : (Functor catC catC (fun a: catC => F (F a)) (fun a b f => fmapD _ _ (fmapD _ _ f))))
+                                    (epsilon : forall (a: catC), (arrow catC (id a) (F a)))
+                                    (delta   : forall (a: catC), (arrow catC (F (F a)) (F a)))
+                                       (nt1: NaturalTransformation catC catC F id fmapD fmapId D Id epsilon)
+                                       (nt2: NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD (fun a b f => fmapD _ _ (fmapD _ _ f)) D D2 delta)
+                                          (CM: coMonad catC F fmapId fmapD Id D D2 epsilon delta nt1 nt2) : (Category).
 Proof. refine (@mk_Category (catC)
                             (fun a b => (@arrow catC (id b) (F a)))
                             (fun a => epsilon a) 
                             (fun a b c f g => g o (fmapD _ _ f) o (delta a))
-_ ). intros. simpl. destruct nt1, nt2. rewrite <- assoc. unfold id in *. remember (comm_diag1 _ _ f).
-destruct H. admit. (* to be proven *) Qed.
-Check coKleisli.
-
-(** mind these admits above. TODO:= prove associativity for Kleisli and coKleisli compositions. **)
-
-(*
-Class Kleisli `(catC: Category) (F: catC -> catC)
-              (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
-              (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-              (fmapT2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (Id  : (Functor catC catC id fmapId))
-                        (T   : (Functor catC catC F fmapT))
-                        (T2  : (Functor catC catC (fun a: catC => F (F a)) fmapT2))
-                        (eta : forall (a: catC), (arrow catC (F a) (id a)))
-                        (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
-                          (nt1: NaturalTransformation catC catC id F fmapId fmapT Id T eta) 
-                          (nt2: NaturalTransformation catC catC (fun a: catC => F (F a)) F fmapT2 fmapT T2 T mu)
-                          `(@Monad catC F fmapId fmapT fmapT2 Id T T2 eta mu nt1 nt2) : Type :=
-{ 
-   Kl_obj := obj catC;
-   Kl_arrow (a b: catC) (f: arrow catC (F b) a) := arrow _ b a;
-   Kl_identity (a: catC) := eta a;
-   Kl_comp (a b c: catC) (f: arrow catC (F b) a) (g: arrow catC (F c) b):= (mu c) o (fmapT _ _ g) o f;
-   Kl_comp_assoc (a b c d: catC) (f : arrow catC (F b) a) (g : arrow catC (F c) b) (h : arrow catC (F d) c):= 
-      (Kl_comp _ _ _  (Kl_comp _ _ _ f g) h) = (Kl_comp _ _ _  f (Kl_comp _ _ _ g h))
-}.
-Check Kleisli.
-
-
-Class coKleisli (catC: Category) (F: catC -> catC)
-                (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
-                (fmapD  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-                (fmapD2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (Id      : (Functor catC catC id fmapId))
-                        (D       : (Functor catC catC F fmapD))
-                        (D2      : (Functor catC catC (fun a: catC => F (F a)) fmapD2))
-                        (epsilon : forall (a: catC), (arrow catC (id a) (F a)))
-                        (delta   : forall (a: catC), (arrow catC (F (F a)) (F a)))
-                          (nt1: NaturalTransformation catC catC F id fmapD fmapId D Id epsilon)
-                          (nt2: NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD fmapD2 D D2 delta)
-                            `(@coMonad catC F fmapId fmapD fmapD2 Id D D2 epsilon delta nt1 nt2) : Type :=
-{ 
-   coKl_obj := obj catC;
-   coKl_arrow (a b: catC) (f: arrow catC b (F a)) := arrow _ b a;
-   coKl_identity (a: catC) := epsilon a;
-   coKl_comp (a b c: catC) (f: arrow catC b (F a)) (g: arrow catC c (F b)):= g o (fmapD _ _ f) o (delta a);
-   coKl_comp_assoc (a b c d: catC) (f : arrow catC b (F a)) (g : arrow catC c (F b)) (h : arrow catC d (F c)):= 
-      (coKl_comp _ _ _  (coKl_comp _ _ _ f g) h) = (coKl_comp _ _ _  f (coKl_comp _ _ _ g h))
-}.
-Check coKleisli.
-
-*)
+                            _ ).
+ (*to dualize*) admit. (*mind the admit here*) Qed.
+Check coKleisli_Category.
 
 End Make.

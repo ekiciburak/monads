58e00f0b2c7560551a107eb655b146561ced4f84
ekiciburak <ekcburak@hotmail.com>
2016-04-11 11:52:13 -0500

    comonads.

--- src/monads.v
+++ src/monads.v
@@ -25,29 +25,23 @@ Class Monad (catC: Category) (F: catC -> catC)
   }.
 Check Monad.
 
-(*TODO:= add Kleisli and coKleisli categories *)
-
-Class Kleisli `(catC: Category) (F: catC -> catC)
+Class coMonad (catC: Category) (F: catC -> catC)
               (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
-              (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-              (fmapT2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (IdF : (Functor catC catC id fmapId))
-                        (T   : (Functor catC catC F fmapT))
-                        (T2  : (Functor catC catC (fun a: catC => F (F a)) fmapT2))
-                        (eta : forall (a: catC), (arrow catC (F a) (id a)))
-                        (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
-                          (nt1: NaturalTransformation catC catC id F fmapId fmapT IdF T eta) 
-                          (nt2: NaturalTransformation catC catC (fun a: catC => F (F a)) F fmapT2 fmapT T2 T mu)
-                          `(@Monad catC F fmapId fmapT fmapT2 IdF T T2 eta mu nt1 nt2) : Type :=
-{ 
-   Kl_obj := obj catC;
-   Kl_arrow (a b: catC) (f: arrow catC (F b) a) := arrow _ b a;
-   Kl_comp (a b c: catC) (f: arrow catC (F b) a) (g: arrow catC (F c) b):= (mu c) o (fmapT _ _ g) o f;
-   Kl_comp_assoc (a b c d: catC) (f : arrow catC (F b) a) (g : arrow catC (F c) b) (h : arrow catC (F d) c):= 
-      (Kl_comp _ _ _  (Kl_comp _ _ _ f g) h) = (Kl_comp _ _ _  f (Kl_comp _ _ _ g h))
-}.
-Check Kleisli.
-Check Kl_arrow.
-
+              (fmapD  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
+              (fmapD2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
+                        (IdD     : (Functor catC catC id fmapId))
+                        (D       : (Functor catC catC F fmapD))
+                        (D2      : (Functor catC catC (fun a: catC => F (F a)) fmapD2))
+                        (epsilon : forall (a: catC), (arrow catC (id a) (F a)))
+                        (delta   : forall (a: catC), (arrow catC (F (F a)) (F a)))
+                          `(@NaturalTransformation catC catC F id fmapD fmapId D IdD epsilon)
+                          `(@NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD fmapD2 D D2 delta): Type :=
+  {
+    cm_comm_diagram1    : forall (a: catC), (fmapD _ _ (delta a)) o (delta a) = (delta (F a)) o (delta a);
+    cm_comm_diagram2    : forall (a: catC), (fmapD _ _ (epsilon a)) o (delta a) = (epsilon (F a)) o (delta a);
+    cm_comm_diagram_b1  : forall (a: catC), (fmapD _ _ (epsilon a)) o (delta a) = (identity catC (F a));
+    cm_comm_diagram_b2  : forall (a: catC), (epsilon (F a)) o (delta a) = (identity catC (F a))
+  }.
+Check coMonad.
 
 End Make.

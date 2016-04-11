36d18ce0f6d247a7b867f10646d82cd25c0351ea
ekiciburak <ekcburak@hotmail.com>
2016-04-11 16:53:27 -0500

    Re-organized: the functor T^2.

--- src/monads.v
+++ src/monads.v
@@ -11,14 +11,13 @@ Definition id {catC: Category} (a: catC) := a.
 Class Monad (catC: Category) (F: catC -> catC)
             (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
             (fmapT  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-            (fmapT2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
                         (Id  : (Functor catC catC id fmapId))
                         (T   : (Functor catC catC F fmapT))
-                        (T2  : (Functor catC catC (fun a: catC => F (F a)) fmapT2))
+                        (T2  : (Functor catC catC (fun a: catC => F (F a)) (fun a b f => fmapT _ _ (fmapT _ _ f))))
                         (eta : forall (a: catC), (arrow catC (F a) (id a)))
                         (mu  : forall (a: catC), (arrow catC (F a) (F (F a))))
                           `(@NaturalTransformation catC catC id F fmapId fmapT Id T eta)
-                          `(@NaturalTransformation catC catC (fun a: catC => F (F a)) F fmapT2 fmapT T2 T mu): Type :=
+                          `(@NaturalTransformation catC catC (fun a: catC => F (F a)) F (fun a b f => fmapT _ _ (fmapT _ _ f)) fmapT T2 T mu): Type :=
   {
     comm_diagram1   : forall (a: catC), (mu a) o (fmapT _ _  (mu a)) = (mu a) o (mu (F a));
     comm_diagram2   : forall (a: catC), (mu a) o (fmapT _ _ (eta a)) = (mu a) o (eta (F a));
@@ -30,14 +29,13 @@ Check Monad.
 Class coMonad (catC: Category) (F: catC -> catC)
               (fmapId : forall (a b: catC) (f: arrow catC b a), (arrow catC (id b) (id a)))
               (fmapD  : forall (a b: catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-              (fmapD2 : forall (a b: catC) (f: arrow catC b a), (arrow catC (F (F b)) (F (F a))))
-                        (Id     : (Functor catC catC id fmapId))
+                        (Id      : (Functor catC catC id fmapId))
                         (D       : (Functor catC catC F fmapD))
-                        (D2      : (Functor catC catC (fun a: catC => F (F a)) fmapD2))
+                        (D2      : (Functor catC catC (fun a: catC => F (F a)) (fun a b f => fmapD _ _ (fmapD _ _ f))))
                         (epsilon : forall (a: catC), (arrow catC (id a) (F a)))
                         (delta   : forall (a: catC), (arrow catC (F (F a)) (F a)))
                           `(@NaturalTransformation catC catC F id fmapD fmapId D Id epsilon)
-                          `(@NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD fmapD2 D D2 delta): Type :=
+                          `(@NaturalTransformation catC catC F (fun a: catC => F (F a)) fmapD (fun a b f => fmapD _ _ (fmapD _ _ f)) D D2 delta): Type :=
   {
     cm_comm_diagram1    : forall (a: catC), (fmapD _ _ (delta a)) o (delta a) = (delta (F a)) o (delta a);
     cm_comm_diagram2    : forall (a: catC), (fmapD _ _ (epsilon a)) o (delta a) = (epsilon (F a)) o (delta a);

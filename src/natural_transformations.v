c929a35c075a3176b71cc0a730812d6552d73835
ekiciburak <ekcburak@hotmail.com>
2016-04-12 11:00:10 -0500

    re-organization of the category structure and its implications.

--- src/natural_transformations.v
+++ src/natural_transformations.v
@@ -20,10 +20,6 @@ Class NaturalTransformation (catC catD: Category) (F G: catC -> catD)
   }.
 Check NaturalTransformation.
 
-(* TODO:= these might be obligations of the Category record *)
-Axiom identity_f: forall (catC: Category) (a b: catC) (f: arrow catC b a), (@identity catC b) o f = f.
-Axiom f_identity: forall (catC: Category) (a b: catC) (f: arrow catC b a), f o (@identity catC a) = f.
-
 Program Instance IdentityNaturalTransformation `(catC: Category) `(catD: Category) (F G: catC -> catD) 
                             (fmapF : forall (a b: catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
                             `(FunctF: @Functor catC catD F fmapF):

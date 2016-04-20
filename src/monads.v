d280119245aea0a266449cefb5f454a182e27449
ekiciburak <ekcburak@hotmail.com>
2016-04-19 20:42:28 -0500

    better treatment of natural transformations & clean-up

--- src/monads.v
+++ src/monads.v
@@ -6,79 +6,47 @@ Require notation categories prods_pullbacks functors natural_transformations.
 Module Make(Import M: notation.T).
  Module Export monads_exp := natural_transformations.Make(M).
 
+
 Class Monad (catC: Category) (F: obj catC -> obj catC)
             (fmapT  : forall (a b: obj catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
                         (Id  : (Functor catC catC id (fun a b f => (@idf catC a b f))))
                         (T   : (Functor catC catC F fmapT))
                         (T2  : (Functor catC catC (fun a: obj catC => F (F a)) (fun a b f => fmapT _ _ (fmapT _ _ f))))
-                        (eta : forall (a: obj catC), (arrow catC (F a) (id a)))
-                        (mu  : forall (a: obj catC), (arrow catC (F a) (F (F a))))
-                          (nt1 :@NaturalTransformation catC catC id F (fun a b f => (@idf catC a b f)) fmapT Id T eta)
-                          (nt2 :@NaturalTransformation catC catC (fun a: obj catC => F (F a)) F (fun a b f => fmapT _ _ (fmapT _ _ f)) fmapT T2 T mu): Type :=
+                        (nt1 :@NaturalTransformation catC catC id F (fun a b f => (@idf catC a b f)) fmapT Id T)
+                        (nt2 :@NaturalTransformation catC catC (fun a: obj catC => F (F a)) F (fun a b f => fmapT _ _ (fmapT _ _ f)) fmapT T2 T): Type :=
   {
-    comm_diagram1   : forall (a: obj catC), (mu a) o (fmapT _ _  (mu a)) = (mu a) o (mu (F a));
-    comm_diagram2   : forall (a: obj catC), (mu a) o (fmapT _ _ (eta a)) = (mu a) o (eta (F a));
-    comm_diagram2_b1: forall (a: obj catC), (mu a) o (fmapT _ _ (eta a)) =  (identity catC (F a));
-    comm_diagram2_b2: forall (a: obj catC), (mu a) o (eta (F a)) =  (identity catC (F a))
+    comm_diagram1   : forall (a: obj catC), (@trans _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _  ((@trans _ _ _ _ _ _ _ _ nt2 a))) 
+                                            = 
+                                             (@trans _ _ _ _ _ _ _ _ nt2 a) o (@trans _ _ _ _ _ _ _ _ nt2 (F a));
+    comm_diagram2   : forall (a: obj catC), (@trans _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _ (@trans _ _ _ _ _ _ _ _ nt1 a)) 
+                                            = 
+                                             (@trans _ _ _ _ _ _ _ _ nt2 a) o (@trans _ _ _ _ _ _ _ _ nt1 (F a));
+    comm_diagram2_b1: forall (a: obj catC), (@trans _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _ (@trans _ _ _ _ _ _ _ _ nt1 a))
+                                            =  (identity catC (F a));
+    comm_diagram2_b2: forall (a: obj catC), (@trans _ _ _ _ _ _ _ _ nt2 a) o (@trans _ _ _ _ _ _ _ _ nt1 (F a)) 
+                                            = (identity catC (F a))
   }.
 Check Monad.
-
-
-Class Monad2 (catC: Category) (F: obj catC -> obj catC)
-            (fmapT  : forall (a b: obj catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-                        (Id  : (Functor catC catC id (fun a b f => (@idf catC a b f))))
-                        (T   : (Functor catC catC F fmapT))
-                        (T2  : (Functor catC catC (fun a: obj catC => F (F a)) (fun a b f => fmapT _ _ (fmapT _ _ f))))
-                        (nt1 :@NaturalTransformation2 catC catC id F (fun a b f => (@idf catC a b f)) fmapT Id T)
-                        (nt2 :@NaturalTransformation2 catC catC (fun a: obj catC => F (F a)) F (fun a b f => fmapT _ _ (fmapT _ _ f)) fmapT T2 T): Type :=
-  {
-    comm_diagram1'   : forall (a: obj catC), (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _  ((@trans2 _ _ _ _ _ _ _ _ nt2 a))) 
-                                             = 
-                                             (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (@trans2 _ _ _ _ _ _ _ _ nt2 (F a));
-    comm_diagram2'   : forall (a: obj catC), (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _ (@trans2 _ _ _ _ _ _ _ _ nt1 a)) 
-                                             = 
-                                             (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (@trans2 _ _ _ _ _ _ _ _ nt1 (F a));
-    comm_diagram2_b1': forall (a: obj catC), (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (fmapT _ _ (@trans2 _ _ _ _ _ _ _ _ nt1 a)) =  (identity catC (F a));
-    comm_diagram2_b2': forall (a: obj catC), (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (@trans2 _ _ _ _ _ _ _ _ nt1 (F a)) =  (identity catC (F a))
-  }.
-Check Monad2.
-
 
 Class coMonad (catC: Category) (F: obj catC -> obj catC)
               (fmapD  : forall (a b: obj catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
                         (Id      : (Functor catC catC id (fun a b f => (@idf catC a b f))))
                         (D       : (Functor catC catC F fmapD))
                         (D2      : (Functor catC catC (fun a: obj catC => F (F a)) (fun a b f => fmapD _ _ (fmapD _ _ f))))
-                        (epsilon : forall (a: obj catC), (arrow catC (id a) (F a)))
-                        (delta   : forall (a: obj catC), (arrow catC (F (F a)) (F a)))
-                          `(@NaturalTransformation catC catC F id fmapD (fun a b f => (@idf catC a b f)) D Id epsilon)
-                          `(@NaturalTransformation catC catC F (fun a: obj catC => F (F a)) fmapD (fun a b f => fmapD _ _ (fmapD _ _ f)) D D2 delta): Type :=
+                        (nt1     : @NaturalTransformation catC catC F id fmapD (fun a b f => (@idf catC a b f)) D Id)
+                        (nt2     : @NaturalTransformation catC catC F (fun a: obj catC => F (F a)) fmapD (fun a b f => fmapD _ _ (fmapD _ _ f)) D D2): Type :=
   {
-    cm_comm_diagram1    : forall (a: obj catC), (fmapD _ _ (delta a)) o (delta a) = (delta (F a)) o (delta a);
-    cm_comm_diagram2    : forall (a: obj catC), (fmapD _ _ (epsilon a)) o (delta a) = (epsilon (F a)) o (delta a);
-    cm_comm_diagram_b1  : forall (a: obj catC), (fmapD _ _ (epsilon a)) o (delta a) = (identity catC (F a));
-    cm_comm_diagram_b2  : forall (a: obj catC), (epsilon (F a)) o (delta a) = (identity catC (F a))
+    cm_comm_diagram1    : forall (a: obj catC), (fmapD _ _ (@trans _ _ _ _ _ _ _ _ nt2 a)) o (@trans _ _ _ _ _ _ _ _ nt2 a) 
+                                                = 
+                                                (@trans _ _ _ _ _ _ _ _ nt2 (F a)) o (@trans _ _ _ _ _ _ _ _ nt2 a);
+    cm_comm_diagram2    : forall (a: obj catC), (fmapD _ _ (@trans _ _ _ _ _ _ _ _ nt1 a)) o (@trans _ _ _ _ _ _ _ _ nt2 a) 
+                                                = 
+                                                (@trans _ _ _ _ _ _ _ _ nt1 (F a)) o (@trans _ _ _ _ _ _ _ _ nt2 a);
+    cm_comm_diagram_b1  : forall (a: obj catC), (fmapD _ _ (@trans _ _ _ _ _ _ _ _ nt1 a)) o (@trans _ _ _ _ _ _ _ _ nt2 a)
+                                                = (identity catC (F a));
+    cm_comm_diagram_b2  : forall (a: obj catC), (@trans _ _ _ _ _ _ _ _ nt1 (F a)) o (@trans _ _ _ _ _ _ _ _ nt2 a)
+                                                = (identity catC (F a))
   }.
 Check coMonad.
-
-
-Class coMonad2 (catC: Category) (F: obj catC -> obj catC)
-              (fmapD  : forall (a b: obj catC) (f: arrow catC b a), (arrow catC (F b) (F a)))
-                        (Id      : (Functor catC catC id (fun a b f => (@idf catC a b f))))
-                        (D       : (Functor catC catC F fmapD))
-                        (D2      : (Functor catC catC (fun a: obj catC => F (F a)) (fun a b f => fmapD _ _ (fmapD _ _ f))))
-                        (nt1     : @NaturalTransformation2 catC catC F id fmapD (fun a b f => (@idf catC a b f)) D Id)
-                        (nt2     : @NaturalTransformation2 catC catC F (fun a: obj catC => F (F a)) fmapD (fun a b f => fmapD _ _ (fmapD _ _ f)) D D2): Type :=
-  {
-    cm_comm_diagram1'    : forall (a: obj catC), (fmapD _ _ (@trans2 _ _ _ _ _ _ _ _ nt2 a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a) 
-                                                = 
-                                                (@trans2 _ _ _ _ _ _ _ _ nt2 (F a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a);
-    cm_comm_diagram2'    : forall (a: obj catC), (fmapD _ _ (@trans2 _ _ _ _ _ _ _ _ nt1 a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a) 
-                                                = 
-                                                (@trans2 _ _ _ _ _ _ _ _ nt1 (F a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a);
-    cm_comm_diagram_b1'  : forall (a: obj catC), (fmapD _ _ (@trans2 _ _ _ _ _ _ _ _ nt1 a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a) = (identity catC (F a));
-    cm_comm_diagram_b2'  : forall (a: obj catC), (@trans2 _ _ _ _ _ _ _ _ nt1 (F a)) o (@trans2 _ _ _ _ _ _ _ _ nt2 a) = (identity catC (F a))
-  }.
-Check coMonad2.
 
 End Make.
--- src/natural_transformations.v
+++ src/natural_transformations.v
@@ -8,53 +8,31 @@ Module Make(Import M: notation.T).
 
 Definition id {catC: Category} (a: obj catC) := a.
 
+Theorem rcancel:  forall {catC: Category} {a b c: obj catC} (f g: arrow _ c b) (h: arrow _ b a), f = g -> f o h = g o h.
+Proof. intros. rewrite H. reflexivity. Qed.
+
 Class NaturalTransformation (catC catD: Category) (F G: obj catC -> obj catD) 
                             (fmapF : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
                             (fmapG : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (G b) (G a)))
                             `(@Functor catC catD F fmapF) 
-                            `(@Functor catC catD G fmapG)
-                            (trans : forall (a: obj catC), (arrow catD (G a) (F a))): Type :=
+                            `(@Functor catC catD G fmapG): Type :=
   mk_nt
-  {  
-    comm_diag: forall {a b: obj catC} (f: arrow catC b a), fmapG _ _ f o trans a = trans b o fmapF _ _ f
+  {
+    trans    :  forall (a: obj catC), (arrow catD (G a) (F a));
+    comm_diag2:  forall {a b: obj catC} (f: arrow catC b a), fmapG _ _ f o trans a  = trans b o fmapF _ _ f
   }.
 Check NaturalTransformation.
-
-Class NaturalTransformation2 (catC catD: Category) (F G: obj catC -> obj catD) 
-                            (fmapF : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
-                            (fmapG : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (G b) (G a)))
-                            `(@Functor catC catD F fmapF) 
-                            `(@Functor catC catD G fmapG): Type :=
-  mk_nt2
-  {
-    trans2    :  forall (a: obj catC), (arrow catD (G a) (F a));
-    comm_diag2:  forall {a b: obj catC} (f: arrow catC b a), fmapG _ _ f o trans2 a  = trans2 b o fmapF _ _ f
-  }.
-Check NaturalTransformation2.
-Check trans2.
+Check trans.
 
 Definition IdentityNaturalTransformation `(catC: Category) `(catD: Category) (F: obj catC -> obj catD) 
                             (fmapF : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
                             `(FunctF: @Functor catC catD F fmapF): 
-                              `(@NaturalTransformation catC catD F F fmapF fmapF FunctF FunctF 
-                                   (fun a => (@identity catD (F a)))).
-Proof. refine (@mk_nt catC catD F F fmapF fmapF FunctF FunctF  (fun a => (@identity catD (F a))) _ ).
+                              `(@NaturalTransformation catC catD F F fmapF fmapF FunctF FunctF).
+Proof. refine (@mk_nt catC catD F F fmapF fmapF FunctF FunctF  (fun a => (@identity catD (F a))) _).
 intros. rewrite identity_f. rewrite f_identity. reflexivity. Defined.
 Check IdentityNaturalTransformation.
 
-Definition IdentityNaturalTransformation2 `(catC: Category) `(catD: Category) (F: obj catC -> obj catD) 
-                            (fmapF : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
-                            `(FunctF: @Functor catC catD F fmapF): 
-                              `(@NaturalTransformation2 catC catD F F fmapF fmapF FunctF FunctF).
-Proof. refine (@mk_nt2 catC catD F F fmapF fmapF FunctF FunctF  (fun a => (@identity catD (F a))) _).
-intros. rewrite identity_f. rewrite f_identity. reflexivity. Defined.
-Check IdentityNaturalTransformation2.
-
-Theorem rcancel:  forall {catC: Category} {a b c: obj catC} (f g: arrow _ c b) (h: arrow _ b a), f = g -> f o h = g o h.
-Proof. intros. rewrite H. reflexivity. Qed.
-
-
-Definition Compose_NaturalTransformations2 (catC catD catE: Category) 
+Definition Compose_NaturalTransformations (catC catD catE: Category) 
                                           (F G H   : obj catC -> obj catD)
                                           (fmapF   : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (F b) (F a)))
                                           (fmapG   : forall (a b: obj catC) (f: arrow catC b a), (arrow catD (G b) (G a)))
@@ -62,11 +40,11 @@ Definition Compose_NaturalTransformations2 (catC catD catE: Category)
                                           (FunctF  : @Functor catC catD F fmapF)
                                           (FunctG  : @Functor catC catD G fmapG) 
                                           (FunctH  : @Functor catC catD H fmapH)
-                                          (nt1     : @NaturalTransformation2 catC catD F G fmapF fmapG FunctF FunctG)
-                                          (nt2     : @NaturalTransformation2 catC catD G H fmapG fmapH FunctG FunctH):
-                                             `(@NaturalTransformation2 catC catD F H fmapF fmapH FunctF FunctH).
-Proof. refine (@mk_nt2 _ _ _ _ _ _ FunctF FunctH
-                                   (fun a: obj catC =>  (@trans2 _ _ _ _ _ _ _ _ nt2 a) o (@trans2 _ _ _ _ _ _ _ _ nt1 a)) _).
+                                          (nt1     : @NaturalTransformation catC catD F G fmapF fmapG FunctF FunctG)
+                                          (nt2     : @NaturalTransformation catC catD G H fmapG fmapH FunctG FunctH):
+                                             `(@NaturalTransformation catC catD F H fmapF fmapH FunctF FunctH).
+Proof. refine (@mk_nt _ _ _ _ _ _ FunctF FunctH
+                                   (fun a: obj catC =>  (@trans _ _ _ _ _ _ _ _ nt2 a) o (@trans _ _ _ _ _ _ _ _ nt1 a)) _).
        intros. destruct nt1, nt2. simpl.
          rewrite <- assoc. rewrite <- comm_diag3.
          repeat rewrite assoc. apply rcancel. apply comm_diag4.
